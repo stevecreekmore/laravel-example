8ee9ea37425ccb189c88f272f30722a5b20316f1
Steven Creekmore <creekmore108@gmail.com>
2025-09-20 10:53:24 -0500

    WIP

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use function Pest\Laravel\artisan;
+
 it('can test', function () {
-    expect(true)->toBeTrue();
+    artisan(SteveCreekmore\LaravelExample\Commands\LaravelExampleCommand::class)
+        ->expectsOutput('All done')
+        ->assertExitCode(\Illuminate\Console\Command::SUCCESS);
 });
